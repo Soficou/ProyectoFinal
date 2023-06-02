d16e071fcda34814675342b6987e3aaf47d6bbb4
MrPraxy <angelmoralesmotos@gmail.com>
2023-06-02 17:49:51 -0600

    Version 3

---
+++ Proyecto Progra xdxd2.cpp
@@ -0,0 +1,130 @@
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <ctime>
+	
+#ifdef _WIN32
+#include <windows.h>
+#endif
+	
+	// Función para convertir un entero en una cadena
+	std::string intToString(int number) {
+	std::stringstream ss;
+	ss << std::setw(4) << std::setfill(' ') << number;
+	return ss.str();
+}
+	
+	// Función para ordenar la matriz
+	void ordenarMatriz(std::vector<std::vector<std::string>>& matriz) {
+		int filas = matriz.size();
+		int columnas = matriz[0].size();
+		
+		for (int i = 0; i < filas; ++i) {
+			for (int j = 0; j < columnas; ++j) {
+				for (int k = 0; k < filas; ++k) {
+					for (int l = 0; l < columnas; ++l) {
+						int valorActual = std::stoi(matriz[i][j]);
+						int valorSiguiente = std::stoi(matriz[k][l]);
+						if (valorActual > valorSiguiente) {
+							std::swap(matriz[i][j], matriz[k][l]);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	// Función para limpiar la pantalla en Windows
+	void clearScreen() {
+#ifdef _WIN32
+		std::system("cls");
+#else
+		std::system("clear");
+#endif
+	}
+	
+	int main() {
+		// Inicializar el generador de números aleatorios con una semilla basada en el tiempo actual
+		srand(time(0));
+		
+		// Definir las dimensiones de la matriz
+		const int filas = 5;
+		const int columnas = 5;
+		
+		// Crear una matriz vacía utilizando vectores anidados
+		std::vector<std::vector<std::string>> matriz(filas, std::vector<std::string>(columnas, "    "));
+		
+		// Definir el rango de los números aleatorios
+		int min = 1;
+		int max = 10;
+		
+		// Generar números aleatorios del 1 al 10 y llenar la matriz
+		for (int i = 0; i < filas; ++i) {
+			for (int j = 0; j < columnas; ++j) {
+				int numero = rand() % (max - min + 1) + min;
+				matriz[i][j] = intToString(numero);
+			}
+		}
+		
+		// Ordenar la matriz
+		ordenarMatriz(matriz);
+		
+		// Posición inicial del carácter representativo
+		int posX = 0;
+		int posY = 0;
+		
+		// Bucle para mover el carácter representativo
+		char character = '>';
+		
+		while (true) {
+			// Limpiar la pantalla
+			clearScreen();
+			
+			// Mostrar la matriz con el carácter representativo
+			for (int i = 0; i < filas; ++i) {
+				std::cout << "|";
+				for (int j = 0; j < columnas; ++j) {
+					if (i == posY && j == posX) {
+						std::cout << " " << character << "  |";
+					} else {
+						std::cout << std::setw(5) << matriz[i][j] << " |";
+					}
+				}
+				std::cout << std::endl;
+				
+				if (i < filas - 1) {
+					std::cout << "|------|------|------|------|------|" << std::endl;
+				}
+			}
+			
+			// Obtener la entrada del usuario
+			std::cout << "Presione W/A/S/D para mover el carácter representativo. Presione Q para salir." << std::endl;
+			char input;
+			std::cin >> input;
+			
+			// Actualizar la posición del carácter representativo
+			if (input == 'W' || input == 'w') {
+				if (posY > 0) {
+					--posY;
+				}
+			} else if (input == 'A' || input == 'a') {
+				if (posX > 0) {
+					--posX;
+				}
+			} else if (input == 'S' || input == 's') {
+				if (posY < filas - 1) {
+					++posY;
+				}
+			} else if (input == 'D' || input == 'd') {
+				if (posX < columnas - 1) {
+					++posX;
+				}
+			} else if (input == 'Q' || input == 'q') {
+				break;
+			}
+		}
+		
+		return 0;
+	}
